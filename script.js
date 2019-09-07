20389492f87ab9bf656679345e7c1498fe25d8c0
staffsterr2000 <rosenkostas@gmail.com>
2019-09-07 09:56:45 +0300

    old up-date

--- script.js
+++ script.js
@@ -2,29 +2,38 @@
 
 var t= 0;
 var checkStart= 0;
+var checkStop= 0;
 
 var timerNode= document.createTextNode("0");
 var timer= document.createElement("p");
 timer.appendChild(timerNode);
 document.body.appendChild(timer);
 
-function time() {
-  t++;
-  timer.innerHTML= t;
-}
-
-function start() {
-  checkStart++;
-  if(checkStart == 1){
-    var interval= setInterval(time, 1000);
+function time(class) {
+  if(class == 'start') {
+    checkStart++;
+    if(checkStart == 1)
+    {
+      var interval= setInterval( function time() {timer.innerHTML= t++; }, 1000);
+    }
   }
-}
+  else {
+    alert("WTF");
+  }
 
-function stop() {
-  clearInrerval(interval);
-  checkStart= 0;
+  // else if(class == "stop") {
+  //   checkStop++;
+  //   if(checkStop == 1) {
+  //     clearInterval(interval);
+  //     timer.innerHTML= 0;
+  //     checkStart= 0;
+  // }
+  // }
 }
-
-// //pause(){
 //
-// //}
+// // //pause(){
+// //
+// // //}
+
+
+https://youtu.be/Gl8rGASgFAw?t=471
